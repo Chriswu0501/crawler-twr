54028b536a56115ce06168893dea90b5afe013c0
Chris Wu <wakeup0501@gmail.com>
2021-08-14 12:40:38 +0800

    update input function

--- crawler-twr.py
+++ crawler-twr.py
@@ -4,6 +4,11 @@ import time
 
 tr_url = 'https://tip.railway.gov.tw/tra-tip-web/tip'
 station_dic = {}
+start_station = input('請輸入出發站點: ')
+end_station = input('請輸入抵達站點: ')
+date = input('請輸入出發日期(格式: Y/M/D): ')
+s_time = input('請輸入出發時間(格式: XX:XX): ')
+e_time = input('請輸入抵達站點(格式: XX:XX): ')
 
 def getTrip(url, s_station, e_station, today, start_time, end_time):
     resp = requests.get(url)
@@ -39,4 +44,5 @@ def getTrip(url, s_station, e_station, today, start_time, end_time):
         td_li = ''.join(list(td[0].li.stripped_strings))
         print('車種車次: {}, 出發時間: {}, 抵達時間: {}'.format(td_li, td[1].text, td[2].text))
 
-getTrip(tr_url, '臺北', '臺南', '2021/08/20', '06:00', '20:00')
+if __name__ == '__main__':
+    getTrip(tr_url, start_station, end_station, date, s_time, e_time)
